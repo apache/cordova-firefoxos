879eaeee5a5077939fd55a4461c5b974b4de3347
nicolagreco <notsecurity@gmail.com>
2014-08-01 11:22:38 -0700

    forEach in loops and improved readability

--- bin/lib/b2g.js
+++ bin/lib/b2g.js
@@ -19,56 +19,53 @@ specific language governing permissions and limitations
 under the License.
 */
 
-var	exec = require('shelljs').exec,
-    args  = process.argv,
+var exec = require('shelljs').exec,
     os = process.platform,
     B2G_BIN_OSX = 'b2g/B2G.app/Contents/MacOS/b2g-bin',
-	FX_PROFILES_OSX = 'Library/Application Support/Firefox/Profiles/',
-	B2G_BIN_LINUX = 'b2g/b2g-bin',
-	FX_PROFILES_LINUX = '.mozilla/firefox/',
-	NETSTAT_CMD = 'netstat -lnptu',
-	LSOF_CMD = 'lsof -i -n -P -sTCP:LISTEN';
+    FX_PROFILES_OSX = 'Library/Application Support/Firefox/Profiles',
+    B2G_BIN_LINUX = 'b2g/b2g-bin',
+    FX_PROFILES_LINUX = '.mozilla/firefox',
+    NETSTAT_CMD = 'netstat -lnptu',
+    LSOF_CMD = 'lsof -i -n -P -sTCP:LISTEN';
 
 exports.discoverPorts = function (callback) {
-	
-	var ports = [];
+    
+    var ports = [];
+
+    if (os == 'darwin') {
+        var output = exec(LSOF_CMD, {silent: true}).output;
+        var regex = /^b2g[-bin]?.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:([0-9]*)/;
+        var lines = output.split('\n');
+        lines.forEach(function(line) {
+            var matches = regex.exec(line);
+            if (matches && +matches[1] != 2828)
+                ports.push(+matches[1])
+        })
  
-	if (os == 'darwin') {
-		var output = exec(LSOF_CMD, {silent: true}).output;
-		var regex = /^b2g[-bin]?.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:([0-9]*)/;
-		var lines = output.split('\n');
-		for (var line=0; line < lines.length; line++) {
-			var matches = regex.exec(lines[line]);
-			
-			if (matches && +matches[1] != 2828)
-				ports.push(+matches[1])
-		}
+    } else
+    if (os == 'linux') {
+        var output = exec(NETSTAT_CMD, {silent: true}).output;
+        var regex = /tcp.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:([0-9]+).*LISTEN.*\/b2g[-bin]?/
+        var lines = output.split('\n');
+        lines.forEach(function(line) {
+            var matches = regex.exec(line);
+            if (matches && +matches[1] != 2828)
+                ports.push(+matches[1])
+        })
  
-	} else
-	if (os == 'linux') {
-		var output = exec(NETSTAT_CMD, {silent: true}).output;
-		var regex = /tcp.*[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:([0-9]+).*LISTEN.*\/b2g[-bin]?/
-		var lines = output.split('\n');
-		for (var line=0; line < lines.length; line++) {
-			var matches = regex.exec(lines[line]);
-			
-			if (matches && +matches[1] != 2828)
-				ports.push(+matches[1])
-		}
+    } else {
+        return callback(new Error("OS not supported for running"))
+    }
  
-	} else {
-		return callback(new Error("OS not supported for running"))
-	}
- 
-	callback(null, ports)
+    callback(null, ports)
  
 }
 
 if (require.main === module) {
     (function() {
         exports.discoverPorts(function(err, ports){
-        	if (err) return console.log(err)
-        	console.log("Running FirefoxOS simulators (B2G) on ports", ports)
+            if (err) return console.log(err)
+            console.log("Running FirefoxOS simulators (B2G) on ports", ports)
         });
     })();
 }
